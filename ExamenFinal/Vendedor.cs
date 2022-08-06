cf245a8b8ba26cba473d452008ff0287de6351eb
Chicho <catania.gian@gmail.com>
2022-08-05 23:42:38 -0300

    Clase Vendedor
    Se crea la clase Vendedor con sus atributos.

---
+++ ExamenFinal/Vendedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal
+{
+    internal class Vendedor
+    {
+        private string _name, _apellido;
+        private int _codigoDeVendedor;
+
+        public string Name { get => _name; set => _name = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public int CodigoDeVendedor { get => _codigoDeVendedor; set => _codigoDeVendedor = value; }
+
+    }
+}
